0c9c5cd9791c2c083e30674abaef26e4a440fdf3
peterMoon <mjh9016@gmail.com>
2018-12-26 21:48:59 +0900

    webpack 빌드 관련 수정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,24 @@
 module.exports = {
+    /* entry: '../dist/index.html',
+    output: {
+        path:__dirname + '/dist',
+    },*/
+    outputDir: '../backend/public',
+    assetsDir: '',
+    chainWebpack: config => {
+        config.plugin('html')
+            .tap(args => {
+                args[0].filename = 'index.html'
+                return args
+            })
+    },
     devServer: {
         proxy: {
             '/api': {
                 target: 'http://localhost:3000',
                 changeOrigin: true
             }
-        }
+        },
+        port: 8081
     }
 }
